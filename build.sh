2255212b38019931762527a432d67dd6f70cb121
Ioannis Canellos <iocanel@gmail.com>
2020-11-05 17:23:59 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mvn clean package -DskipTests -Ptekton
+
+kubectl delete -f target/classes/META-INF/dekorate/tekton-task-run.yml
+kubectl delete -f target/classes/META-INF/dekorate/tekton-task.yml
+
+kubectl apply -f target/classes/META-INF/dekorate/tekton-task.yml
+kubectl apply -f target/classes/META-INF/dekorate/tekton-task-run.yml
+
+tkn taskrun logs dekorate-tekton-example-run-now -f
